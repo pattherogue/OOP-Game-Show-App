ae375b0ba36d62891a1140fb0129db15b2638b47
Paolo Gomez <78209142+pattherogue@users.noreply.github.com>
2021-08-09 11:34:19 -0700

    Update Phrase.js

--- js/Phrase.js
+++ js/Phrase.js
@@ -8,34 +8,40 @@ class Phrase {
     constructor(phrase) {
         this.phrase = phrase.toLowerCase();
     }
-}
+
 
 /* class should have the following methods */
 
 /* addPhraseToDisplay() */
 /* adds letter placeholders to the display when game starts */
-addPhraseToDisplay() {
-    const phraseDisplayed = this.phrase;
-    for (let i = 0; i < phraseDisplayed.length; i++) {
-        const letter = phraseDisplayed[i];
-        const phraseUl = document.querySelector('#phrase ul');
-        const characterChosen = document.createElement('li');
-        phraseUl.appendChild(letter);
-        characterChosen.textContent = `${phraseUl}`;
-        if (letter === '') {
-            characterChosen.className = 'space';
-        } else {
-            characterChosen.className = `hide letter ${letter}`;
-        }
+    addPhraseToDisplay() {
+        const phraseDisplay = this.phrase;
+        for (let i = 0; i < phraseDisplay.length; i++) {
+            const letterPlaceHolder = phraseDisplay[i];
+            const phraseUl = document.querySelector('#phrase ul');
+            const characterElement = document.createElement('li');
+            phraseUl.appendChild(letterPlaceHolder);
+            characterElement.textContent = `${phraseUl}`;
+            if (letterPlaceHolder === '') {
+                /* space CSS for spaces */
+                characterElement.className = 'space';
+            } else {
+                /* letter CSS for letters */
+                characterElement.className = `hide letter ${letterPlaceHolder}`;
+            }
+        };
     };
-};
-
-
-
-
 
 /* checkLetter */
 /* check to see if the letter selected matches letter in phrase */
+    checkLetter(letter) {
+        return.this.phrase.includes(letter);
+    };
+
+
+
 
 /* showMatchedLetter () */
 /* reveals letter(s) on the board that matches player selection */
+
+}
