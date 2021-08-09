b4357f5df063a68bea34374ad261771cf3bf17da
Paolo Gomez <78209142+pattherogue@users.noreply.github.com>
2021-08-09 10:57:48 -0700

    Initial commit

---
+++ js/Phrase.js
@@ -0,0 +1,22 @@
+/* Treehouse FSJS Techdegree
+ * Project 4 - OOP Game App
+ * Phrase.js */
+
+/* create Phrase class */
+/* constructor that recieves phrase parameter and inititializes the following */
+class Phrase {
+    constructor(phrase) {
+        this.phrase = phrase.toLowerCase();
+    }
+}
+/* phrase */
+/* class should have the following methods */
+
+/* addPhraseToDisplay() */
+/* adds letter placeholders to the display when game starts */
+
+/* checkLetter */
+/* check to see if the letter selected matches letter in phrase */
+
+/* showMatchedLetter () */
+/* reveals letter(s) on the board that matches player selection */
