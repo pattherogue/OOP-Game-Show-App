2799bb210549592a2ecd02887f3d3e70d38d818b
Paolo Gomez <78209142+pattherogue@users.noreply.github.com>
2021-08-10 11:24:24 -0700

    Update Game.js

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,16 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+/* create the 'game' class */
+class Game {
+    constructor() {
+        /* track the number of missed guesses */
+        this.missed = 0;
+        /* array of five 'Phrase' objects */
+        this.phrase = this.createPhrase();
+        /* 'Phrase' currently in play */
+        this.activePhrase = null;
+        this.startScreenOverlay = document.getElementById('overlay');
+    }
+}
