aec4b6d01b2d7a44f767a67ee1ab649f52d3555c
Paolo Gomez <78209142+pattherogue@users.noreply.github.com>
2021-08-10 11:40:09 -0700

    Update Game.js

--- js/Game.js
+++ js/Game.js
@@ -8,9 +8,30 @@ class Game {
         /* track the number of missed guesses */
         this.missed = 0;
         /* array of five 'Phrase' objects */
-        this.phrase = this.createPhrase();
+        this.phrase = this.storedPhrase();
         /* 'Phrase' currently in play */
         this.activePhrase = null;
         this.startScreenOverlay = document.getElementById('overlay');
-    }
+    };
+
+    storedPhrase() {
+        let storedArray = [
+            newPhrase('Buy low sell high');
+            newPhrase('I am waiting for the pullback');
+            newPhrase('The trend is your friend');
+            newPhrase('When the tide goes out you see who is swimming naked');
+            newPhrase('Investing should be more like watching paint dry or watching grass grow');
+        ]
+
+        return storedArray;
+    };
+
+
+
+
+
+
+    
+
+
 }
