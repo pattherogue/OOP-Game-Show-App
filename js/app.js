c19ad2921e1cfab6946ad3d2f25aa524e8b2dc20
Paolo Gomez <78209142+pattherogue@users.noreply.github.com>
2021-08-13 03:42:16 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,17 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * app.js */
+
+let game = '';
+const startButton = document.getElementsById('btn__reset');
+
+startButton.addEventListener('click', () => {
+    game = new Game;
+    game.startGame();
+});
+
+document.getElementById('property').addEventListener('click', (e) => {
+    if (e.target.tagName === 'BUTTON') {
+        game.handleInteraction(e.target);
+    }
+});
